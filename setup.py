61d66d56eaa52ef2d100c0780bdf804d84a9155f
r <radu@raicea.com>
2018-04-17 17:12:37 -0400

    bump to v0.2.1

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ def readme():
         return f.read()
 
 setup(name='music2storage',
-      version='0.1.5',
+      version='0.2.1',
       description='Downloads music from a service and stores it in a cloud or local storage.',
-      long_description='Currently the only music service is Youtube and the only storage is Google Drive. Soundcloud and Dropbox are planned to be added soon.',
+      long_description='Currently the only music services are Youtube and Soundcloud, and the only storages are Google Drive and Local Storage. Dropbox is planned to be added soon.',
       url='https://github.com/Music-Moo/music2storage',
       author='Radu Raicea',
       author_email='radu@raicea.com',
@@ -27,4 +27,11 @@ setup(name='music2storage',
       python_requires=">=3.6",
       zip_safe=False,
       test_suite='nose.collector',
-      tests_require=['nose'])
+      tests_require=['nose'],
+      classifiers=(
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: Implementation :: CPython'
+      ))
